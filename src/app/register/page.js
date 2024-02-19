65d025d9e0f66eb5c0de6a8187250e162533b0c7
raihannizar24 <raihannizar24@gmail.com>
2024-02-19 23:40:06 +0700

    fix: rapi rapi fitur register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,29 +1,5 @@
-"use client";
+import { Register } from "@/components/Register";
 
-import { useRegister } from "@/components/auth/hooks/useRegister";
-
-export default function RegisterPage() {
-  const { handleSubmitRegister, setName, setEmail, setPassword } =
-    useRegister();
-
-  return (
-    <div>
-      <form onSubmit={handleSubmitRegister}>
-        <input
-          placeholder="name"
-          onChange={(e) => setName(e.target.value)}
-        ></input>
-        <input
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
-          placeholder="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button>Daftar</button>
-      </form>
-    </div>
-  );
+export default function Page() {
+  return <Register />;
 }

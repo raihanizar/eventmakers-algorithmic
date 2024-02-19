ed5b74ffb37890d079641ca044d20720b4f8b848
raihannizar24 <raihannizar24@gmail.com>
2024-02-19 22:07:15 +0700

    fix: merapikan isi & tatak letak kode fitur login

---
+++ src/components/hooks/useLogin.jsx
@@ -0,0 +1,42 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const useLogin = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmitLogin(event) {
+    event.preventDefault();
+
+    try {
+      const res = await fetch("https://eventmakers-api.fly.dev/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const { payload, token } = await res.json();
+
+      // set payload to localstorage
+      localStorage.setItem("user", JSON.stringify(payload));
+
+      // set token to cookie
+      Cookies.set("token", token);
+
+      // jika berhasil login, redirect ke halaman events
+      router.push("/events");
+
+    } catch {
+      // jika gagal login, kembalikan ke halaman login
+      router.push("/");
+    }
+  }
+
+  return { handleSubmitLogin, setEmail, setPassword };
+};

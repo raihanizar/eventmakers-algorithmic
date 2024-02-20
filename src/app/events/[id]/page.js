29e2d519c91062e20220a75caa713528cbf98f9e
raihannizar24 <raihannizar24@gmail.com>
2024-02-20 16:19:14 +0700

    fix: rapi rapi fitur view detail

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -1,41 +1,5 @@
-import 'dotenv/config'
+import { DetailEvents } from "@/components/DetailEvents";
 
-async function getData(id) {
-  try {
-    const response = await fetch(`https://eventmakers-api.fly.dev/events/${id}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.TOKEN}`
-      }
-    })
-    const data = await response.json()
-    return data
-  } catch (error) {
-    return null
-  }
-}
-
-export default async function Page({ params }) {
-  
-  const { message, data } = await getData(params.id)
-
-  return (
-    <main className="p-8 bg-rose-200 flex flex-col gap-y-8">
-
-      <div className="flex flex-col gap-y-4">
-        <div className="flex flex-col gap-y-2">
-          <h2 className="text-2xl">{data.events.title}</h2>
-          <p>{data.events.description}</p>
-          <p>{data.events.dateTime}</p>
-        </div>
-        <div className="flex flex-col gap-x-2">
-          {data.participants.length > 0
-            ? data.participants.map((participant, idx) => (
-              <p key={participant.id} className="text-gray-500 italic">{participant.name}</p>
-            ))
-            : <p className="text-gray-500 italic">no participant</p>
-          }
-        </div>
-      </div>
-    </main>
-  )
+export default function Page({ params }) {
+  return <DetailEvents id={params.id} />;
 }

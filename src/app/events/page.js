f4bed0b195ad8ff4e9ce39583cb73936fc39c5a9
raihannizar24 <raihannizar24@gmail.com>
2024-02-20 13:44:04 +0700

    fix: rapi rapi fitur view all event

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,52 +1,5 @@
-import 'dotenv/config'
+import { ListEvents } from "@/components/ListEvents";
 
-async function getData() {
-  try {
-    const response = await fetch('https://eventmakers-api.fly.dev/events/', {
-      headers: {
-        Authorization: `Bearer ${process.env.TOKEN}`
-      }
-    })
-    const data = await response.json()
-    return data
-  } catch (error) {
-    return null
-  }
-}
-
-
-export default async function Events() {
-
-  const payloadDummy = {
-    id: "ds_V5bTiVsgnx6gk30",
-    name: "ooo",
-    email: "ooo.com",
-    avatar: null
-  }
-
-  const { data: eventData, message: eventMessage } = await getData()
-
-  return (
-    <main className="p-8 bg-rose-200 flex flex-col gap-y-8">
-      <div>Login as: {payloadDummy.name}</div>
-
-      {eventData ? eventData.map((data, idx) => (
-        <div key={idx} className="flex flex-col gap-y-4">
-          <div className="flex flex-col gap-y-2">
-            <h2 className="text-2xl">{data.events.title}</h2>
-            <p>{data.events.description}</p>
-            <p>{data.events.dateTime}</p>
-          </div>
-          <div className="flex flex-col gap-x-2">
-            {data.participants.length > 0
-              ? data.participants.map((participant, idx) => (
-                <p key={participant.id} className="text-gray-500 italic">{participant.name}</p>
-              ))
-              : <p className="text-gray-500 italic">no participant</p>
-            }
-          </div>
-        </div>
-      )) : <div>no data</div>}
-    </main>
-  );
+export default function Page() {
+  return <ListEvents />;
 }
---
+++ src/components/ListEvents.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import Cookies from "js-cookie"
+
+const getEventData = async (token, id = "") => {
+  try {
+    const response = await fetch(`https://eventmakers-api.fly.dev/events/${id}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    const res = await response.json()
+    return res
+  } catch (error) {
+    console.error(error)
+    return {
+      "message": "Fetch events failed",
+      "data": {}
+    }
+  }
+}
+
+export const ListEvents = () => {
+  const [userData, setUserData] = useState(null);
+  const [eventData, setEventData] = useState(null);
+  const [eventMessage, setEventMessage] = useState("");
+
+  useEffect(() => {
+    const payload = localStorage.getItem("user");
+    setUserData(JSON.parse(payload))
+
+    const token = Cookies.get("token");
+    const fetchData = async () => {
+      try {
+        const { data, message } = await getEventData(token);
+        setEventData(data)
+        setEventMessage(message)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <main className="p-8 bg-rose-200 flex flex-col gap-y-8">
+      <div>Login as: {userData ? userData.name : ''}</div>
+
+      {eventData
+        ? eventData.map((data, idx) => (
+          <div key={idx} className="flex flex-col gap-y-4">
+
+            <div className="flex flex-col gap-y-2">
+              <h2 className="text-2xl">{data.events.title}</h2>
+              <p>{data.events.description}</p>
+              <p>{data.events.dateTime}</p>
+            </div>
+
+            <div className="flex flex-col gap-x-2">
+              {data.participants.length > 0
+                ? data.participants.map((participant, idx) => (
+                  <p key={participant.id} className="text-gray-500 italic">{participant.name}</p>
+                ))
+                : <p className="text-gray-500 italic">no participant</p>
+              }
+            </div>
+
+          </div>
+        ))
+        : <div>no data</div>}
+    </main>
+  );
+}

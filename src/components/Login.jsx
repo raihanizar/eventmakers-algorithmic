3164053a9b31d1c2b5b203ffeb10e511909b8be8
dilekilla <dildilpa00@gmail.com>
2024-02-22 16:06:44 +0700

    feat: adding login UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,48 @@
 "use client";
 
+import Link from "next/link";
 import { useLogin } from "./hooks/useLogin";
 
 export const Login = () => {
   const { handleSubmitLogin, setEmail, setPassword } = useLogin();
 
   return (
-    <div>
-      <form onSubmit={handleSubmitLogin} className="bg-emerald-200">
-        <input name="email" onChange={(e) => setEmail(e.target.value)}></input>
-        <input name="password" type="password" onChange={(e) => setPassword(e.target.value)}></input>
-        <button>Login</button>
+    <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
+      <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
+        <h1 className="font-semibold text-2xl">Login</h1>
+        
+        <form onSubmit={handleSubmitLogin} className="space-y-12 w-full sm:w-[400px]">
+          <div className="grid w-full items-center gap-1.5">
+          <input 
+          className="w-full" 
+          required 
+          name="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          >
+          </input>
+          </div>
+        
+          <div className="grid w-full items-center gap-1.5">
+          <input 
+          className="w-full" 
+          required 
+          name="password" 
+          type="password" onChange={(e) => setPassword(e.target.value)}></input>
+          </div>
+        
+          <div className="w-full">
+          <button className="w-full" size="lg">Login</button>
+          </div>
       </form>
+
+        <p className="text-center">
+          Need to create an account? {' '}
+          <Link className="text-indigo-500 hover:underline" href="/useRegister">
+            Create Account
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ import * as jose from "jose";
 
 export async function middleware(request) {
   console.log("ini dari middleware");
-  const token = cookies().get("token").value;
+  const token = cookies().get("token")?.value;
   const secretKey = process.env.SECRET_KEY;
   const secret = new TextEncoder().encode(secretKey);
 

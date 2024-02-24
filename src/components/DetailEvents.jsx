12d3170e597c54a78975a26edef11b7adfae4e9c
raihannizar24 <raihannizar24@gmail.com>
2024-02-24 16:12:14 +0700

    feat: join event

--- src/components/DetailEvents.jsx
+++ src/components/DetailEvents.jsx
@@ -4,22 +4,6 @@ import { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie"
 
-const getEventData = async (token, id = "") => {
-  try {
-    const response = await fetch(`https://eventmakers-api.fly.dev/events/${id}`, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-    const res = await response.json()
-    return res
-  } catch (error) {
-    console.error(error)
-    return {
-      "message": "Fetch events failed",
-      "data": {}
-    }
-  }
-}
-
 export const DetailEvents = ({ id }) => {
   const [userData, setUserData] = useState(null);
   const [eventData, setEventData] = useState(null);
@@ -43,6 +27,54 @@ export const DetailEvents = ({ id }) => {
     fetchData()
   }, [id])
 
+  const getEventData = async (token, id = "") => {
+    try {
+      const response = await fetch(`https://eventmakers-api.fly.dev/events/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        cache: "no-store"
+      })
+      const res = await response.json()
+      return res
+    } catch (error) {
+      console.error(error)
+      return {
+        "message": "Fetch events failed",
+        "data": {}
+      }
+    }
+  }
+
+  const handleJoinEvent = async (id) => {
+    const token = Cookies.get("token");
+    try {
+      const response = await fetch(`https://eventmakers-api.fly.dev/events/${id}/join`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        cache: "no-store",
+        body: JSON.stringify({
+          "name": userData.name,
+          "email": userData.email,
+          "phoneNumber": "123"
+        })
+      })
+      const res = await response.json()
+      window.location.reload() // reload page
+      return res
+    } catch (error) {
+      console.error(error)
+      return {
+        "message": "Join events failed",
+        "data": {}
+      }
+    }
+  }
+
   return (
     <main className="p-8 bg-rose-200 flex flex-col gap-y-8">
       <div>Login as: {userData ? userData.name : ''}</div>
@@ -64,11 +96,13 @@ export const DetailEvents = ({ id }) => {
                 : <p className="text-gray-500 italic">no participant</p>
               }
             </div>
+
+            <button className="bg-slate-800 text-slate-50 p-2 rounded" onClick={() => handleJoinEvent(eventData.events.id)}>join</button>
+            <button className="bg-slate-800 text-slate-50 p-2 rounded" onClick={() => router.back()}>kembali</button>
           </div>
         )
         : <p>no data</p>}
 
-      <button className="bg-slate-800 text-slate-50 p-2 rounded" onClick={() => router.back()}>kembali</button>
     </main>
   )
 }
